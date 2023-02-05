252f923f76488d4fc8d37aa864c3556961c0a126
linelijah3 <elijahlin3@gmail.com>
2023-02-04 20:42:01 -0500

    added initial hand functionality
    Added hand functionality including adding a random card and returning the length of the hand.

---
+++ helpingHand_Back/web-service-gin/hand/hand.go
@@ -0,0 +1,50 @@
+package hand
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// copied Card structure from deck.go for consistency
+type Card struct {
+	val  int
+	suit string
+}
+
+// defining hand as an array of cards separate from the deck
+type Hand struct {
+	ActualHand []Card //the 5 cards in the hand
+	HandType   string //i.e. straight, 4 of a kind, royal flush...
+}
+
+func NewHand(handType string) *Hand {
+	return &Hand{[]Card{}, handType}
+}
+
+// add a card to the hand if hand has less than 5 cards
+func AddCardHand(hand *Hand) {
+	if len(hand.ActualHand) < 5 {
+		//get "random" value from time
+		rand.Seed(time.Now().UnixNano())
+		Number := rand.Intn(14)
+		SuitValue := rand.Intn(4)
+		Suit := ""
+		switch SuitValue {
+		case 0:
+			Suit = "Heart"
+		case 1:
+			Suit = "Diamond"
+		case 2:
+			Suit = "Club"
+		case 3:
+			Suit = "Spade"
+		default:
+			Suit = "Error when adding card to hand"
+		}
+		card := Card{Number, Suit}
+		hand.ActualHand = append(hand.ActualHand, card)
+	} else {
+		fmt.Println("length of hand is already 5")
+	}
+}
--- helpingHand_Back/web-service-gin/main.go
+++ helpingHand_Back/web-service-gin/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"example/web-service-gin/deck"
+	"example/web-service-gin/hand"
 	"fmt"
 )
 
 func main() {
 	currDeck := deck.New()
 	fmt.Println(deck.GetCardIndex(currDeck, 0, "Heart"))
+	currentHand := hand.NewHand("initial")
+	hand.AddCardHand(currentHand)
+	fmt.Println("current length of hand is: ")
+	fmt.Println(len(currentHand.ActualHand))
 }
