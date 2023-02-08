99d9b7b12bb2c675c79d012e6f4029ee59128afe
rickrekkem <58759497+rickrekkem@users.noreply.github.com>
2023-02-07 20:47:40 -0500

    Re-added backend connection material
    I don't even know how I removed it in the first place. Github causes me confusion to no end.

--- helpingHand_Back/web-service-gin/main.go
+++ helpingHand_Back/web-service-gin/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"example/web-service-gin/card"
 	"example/web-service-gin/deck"
 	"example/web-service-gin/hand"
 	"example/web-service-gin/handler"
@@ -11,5 +10,19 @@ import (
 )
 
 func main() {
+	currDeck := deck.NewDeck()
+	fmt.Println(deck.GetCardIndex(currDeck, 0, "Heart"))
+	currentHand := hand.NewHand("initial")
+	hand.AddCardHand(currentHand)
+	fmt.Println("current length of hand is: ")
+	fmt.Println(len(currentHand.ActualHand))
+
+	r := gin.Default()
+	api := r.Group("/api")
+	{
+		api.GET("/ping", handler.PingGet())
+		api.GET("/hand", handler.HandGet(currentHand))
+		api.POST("/hand", handler.HandPost(currentHand))
 	}
+	r.Run("0.0.0.0:5000")
 }
